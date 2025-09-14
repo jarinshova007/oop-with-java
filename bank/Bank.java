ec31452b1e86cb7f28e5c3cfb65f69c835e07b08
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-09-14 17:20:00 +0600

    Deleted bank folder

--- bank/Bank.java
+++
@@ -1,9 +0,0 @@
-import bank.*;
-
-public class Bank {
-    public static void main(String[] args) {
-        Account account1 = new Account();
-        account1.name = "customer1";
-        account1.printInfo();
-    }
-}
--- bank/Main.java
+++
@@ -1,8 +0,0 @@
-import bank.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Account account2 = new Account("customer2");
-        account2.printInfo();
-    }
-}
